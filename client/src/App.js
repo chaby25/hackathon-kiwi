ca4ffbbf12ff332ee7fbaa72e684994db0817ea5
Sergio <sergio.novo@atrapalo.com>
2019-03-29 22:47:18 +0100

    Extract SearchForm to component

--- client/src/App.js
+++ client/src/App.js
@@ -1,47 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
-import InputField from "@kiwicom/orbit-components/lib/InputField";
-import Button from '@kiwicom/orbit-components/es/Button/index';
+import SearchForm from './components/SearchForm';
 
 class App extends Component {
-    constructor() {
-        super();
-        this.state = {origin: 'Barcelona', number_persons: '', departure_date: '', outward_date: ''};
-    }
 
     render() {
         return (
             <div>
-                <div className="form-group">
-                        <InputField
-                            label="Origin"
-                        />
-                        <InputField
-                            label="Number of persons"
-                            type="number"
-                            placeholder="Number of persons"
-                        />
-                        <InputField
-                            label="Departure date"
-                            type="date"
-                        />
-                        <InputField
-                            label="Outward date"
-                            type="date"
-                        />
-                    <Button onClick={this.search}
-                            block={false}
-                            type="primary"
-                            size="normal">
-                        Search
-                    </Button>
-                </div>
+                <SearchForm/>
             </div>
         );
-    }
-
-    search = () => {
-        console.log('search')
     }
 }
 
---
+++ client/src/components/SearchForm.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import InputField from "@kiwicom/orbit-components/lib/InputField";
+import Button from '@kiwicom/orbit-components/es/Button/index';
+
+class SearchForm extends Component {
+
+    constructor() {
+        super();
+        this.state = {origin: 'Barcelona', number_persons: '', departure_date: '', outward_date: ''};
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="form-group">
+                    <InputField
+                        label="Origin"
+                    />
+                    <InputField
+                        label="Number of persons"
+                        type="number"
+                        placeholder="Number of persons"
+                    />
+                    <InputField
+                        label="Departure date"
+                        type="date"
+                    />
+                    <InputField
+                        label="Outward date"
+                        type="date"
+                    />
+                    <Button onClick={this.search}
+                            block={false}
+                            type="primary"
+                            size="normal">
+                        Search
+                    </Button>
+                </div>
+            </div>
+        );
+    }
+
+    search = () => {
+        console.log('search')
+    }
+}
+
+export default SearchForm;
